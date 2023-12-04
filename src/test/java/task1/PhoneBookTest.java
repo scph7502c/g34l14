34243eebde3ab3184c659ceac41b45bd22722ce5
Damian Temimi <damian.temimi@gmail.com>
2023-12-04 21:37:43 +0100

    changes

--- src/test/java/task1/PhoneBookTest.java
+++ src/test/java/task1/PhoneBookTest.java
@@ -3,10 +3,7 @@ package task1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import java.util.Objects;
